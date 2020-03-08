e08cec4ed961b390b577757124afe6a924785a76
Wallace Wallace <dbw5808@rit.edu>
2020-03-08 00:31:25 -0500

    dog works?

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -54,6 +54,19 @@ const readAllCats = (req, res, callback) => {
   Cat.find(callback).lean();
 };
 
+const readAllDogs = (req, res, callback) => {
+  // Call the model's built in find function and provide it a
+  // callback to run when the query is complete
+  // Find has several versions
+  // one parameter is just the callback
+  // two parameters is JSON of search criteria and callback.
+  // That limits your search to only things that match the criteria
+  // The find function returns an array of matching objects
+  // The lean function will force find to return data in the js
+  // object format, rather than the Mongo document format.
+  Dog.find(callback).lean();
+};
+
 
 
 // function to find a specific cat on request.
@@ -78,6 +91,28 @@ const readCat = (req, res) => {
   // Behind the scenes this runs the findOne method.
   // You can find the findByName function in the model file.
   Cat.findByName(name1, callback);
+};
+
+const readDog = (req, res) => {
+  const name1 = req.query.name;
+
+  // function to call when we get objects back from the database.
+  // With Mongoose's find functions, you will get an err and doc(s) back
+  const callback = (err, doc) => {
+    if (err) {
+      return res.status(500).json({ err }); // if error, return it
+    }
+
+    // return success
+    return res.json(doc);
+  };
+
+  // Call the static function attached to CatModels.
+  // This was defined in the Schema in the Model file.
+  // This is a custom static function added to the CatModel
+  // Behind the scenes this runs the findOne method.
+  // You can find the findByName function in the model file.
+  Dog.findByName(name1, callback);
 };
 
 
@@ -134,6 +169,13 @@ const getName = (req, res) => {
   // Since this sends back the data through HTTP
   // you can't send any more data to this user until the next response
   res.json({ name: lastAdded.name });
+};
+
+const getNameDog = (req, res) => {
+  // res.json returns json to the page.
+  // Since this sends back the data through HTTP
+  // you can't send any more data to this user until the next response
+  res.json({ name: lastAddedDog.name });
 };
 
 
@@ -268,6 +310,43 @@ const searchName = (req, res) => {
   });
 };
 
+
+const searchNameDog = (req, res) => {
+  // check if there is a query parameter for name
+  // BUT WAIT!!?!
+  // Why is this req.query and not req.body like the others
+  // This is a GET request. Those come as query parameters in the URL
+  // For POST requests like the other ones in here, those come in a
+  // request body because they aren't a query
+  // POSTS send data to add while GETS query for a page or data (such as a search)
+  if (!req.query.name) {
+    return res.status(400).json({ error: 'Name is required to perform a search' });
+  }
+
+  // Call our Cat's static findByName function.
+  // Since this is a static function, we can just call it without an object
+  // pass in a callback (like we specified in the Cat model
+  // Normally would you break this code up, but I'm trying to keep it
+  // together so it's easier to see how the system works
+  // For that reason, I gave it an anonymous callback instead of a
+  // named function you'd have to go find
+  return Dog.findByName(req.query.name, (err, doc) => {
+    // errs, handle them
+    if (err) {
+      return res.status(500).json({ err }); // if error, return it
+    }
+
+    // if no matches, let them know
+    // (does not necessarily have to be an error since technically it worked correctly)
+    if (!doc) {
+      return res.json({ error: 'No dogs found' });
+    }
+
+    // if a match, send the match back
+    return res.json({ name: doc.name, age: doc.age, breed: doc.breed });
+  });
+};
+
 // function to handle a request to update the last added object
 // this PURELY exists to show you how to update a model object
 // Normally for an update, you'd get data from the client,
@@ -289,6 +368,27 @@ const updateLast = (req, res) => {
 
   // send back the name as a success for now
   savePromise.then(() => res.json({ name: lastAdded.name, beds: lastAdded.bedsOwned }));
+
+  // if save error, just return an error for now
+  savePromise.catch((err) => res.status(500).json({ err }));
+};
+
+
+const updateLastDog = (req, res) => {
+  // Your model is JSON, so just change a value in it.
+  // This is the benefit of ORM (mongoose) and/or object documents (Mongo NoSQL)
+  // You can treat objects just like that - objects.
+  // Normally you'd find a specific object, but we will only
+  // give the user the ability to update our last object
+  lastAddedDog.age++;
+
+  // once you change all the object properties you want,
+  // then just call the Model object's save function
+  // create a new save promise for the database
+  const savePromise = lastAddedDog.save();
+
+  // send back the name as a success for now
+  savePromise.then(() => res.json({ name: lastAddedDog.name, age: lastAddedDog.age, breed: lastAddedDog.breed }));
 
   // if save error, just return an error for now
   savePromise.catch((err) => res.status(500).json({ err }));
@@ -318,10 +418,14 @@ module.exports = {
   page2: hostPage2,
   page3: hostPage3,
   readCat,
+  readDog,
   getName,
   setName,
+  getNameDog,
   setNameDog,
   updateLast,
   searchName,
+  updateLastDog,
+  searchNameDog,
   notFound,
 };
--- server/router.js
+++ server/router.js
@@ -18,6 +18,8 @@ const router = (app) => {
   app.get('/page3', controllers.page3);
   app.get('/getName', controllers.getName);
   app.get('/findByName', controllers.searchName);
+   app.get('/getNameDog', controllers.getNameDog);
+  app.get('/findByNameDog', controllers.searchNameDog);
 
     // whenever someone goes to the site without a path (AKA the home page), call controllers.index
     // For example www.webpage.com
@@ -33,6 +35,7 @@ const router = (app) => {
 
     // When someone POSTS to /updateLast, call controllers.updateLast
   app.post('/updateLast', controllers.updateLast);
+  app.post('/updateLastDog', controllers.updateLastDog);
 };
 
 // export the router function
