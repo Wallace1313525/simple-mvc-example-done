095f9cd006950665f6834f4d70e1fb2787855195
Wallace Wallace <dbw5808@rit.edu>
2020-03-08 00:18:37 -0500

    accidently deleted notFound

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -294,6 +294,21 @@ const updateLast = (req, res) => {
   savePromise.catch((err) => res.status(500).json({ err }));
 };
 
+// function to handle a request to any non-real resources (404)
+// controller functions in Express receive the full HTTP request
+// and get a pre-filled out response object to send
+const notFound = (req, res) => {
+  // res.render takes a name of a page to render.
+  // These must be in the folder you specified as views in your main app.js file
+  // Additionally, you don't need .jade because you registered the file type
+  // in the app.js as jade. Calling res.render('index')
+  // actually calls index.jade. A second parameter of JSON can be passed into
+  // the jade to be used as variables with #{varName}
+  res.status(404).render('notFound', {
+    page: req.url,
+  });
+};
+
 
 
 // export the relevant public controller functions
